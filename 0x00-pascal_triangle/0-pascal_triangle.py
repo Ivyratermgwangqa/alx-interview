f2d35e6a5b319e20e9666c1bdc77aa822216f9bd
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-04-27 22:42:42 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Module that generates Pascal's Triangle for a given number of rows.
 """
 
+
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle with `n` rows.
@@ -16,25 +17,16 @@ def pascal_triangle(n):
     
     if n <= 0:
         return []
-    
     triangle = []
-    
     row = [1]  # First row is always [1]
-    
     for _ in range(n):
         triangle.append(row)  # Add the current row to the triangle
-        
         new_row = []
-        
         new_row.append(1)  # Add the first '1'
-        
         # Compute the elements in between
         for i in range(1, len(row)):
             new_row.append(row[i - 1] + row[i])
-        
         new_row.append(1)  # Add the last '1'
-        
         # Update the row to be the new row
         row = new_row
-    
     return triangle
