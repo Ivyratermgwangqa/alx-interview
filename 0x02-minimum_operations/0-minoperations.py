4f4f0151d0b4db21422d3b5cb7cd51d86d2619f9
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-10 04:51:49 +0200

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -26,9 +26,8 @@ def minOperations(n):
     """
     if n <= 1:
       return 0
-      
-      operations = 0
-      divisor = 2
+    operations = 0
+    divisor = 2
     while divisor <= n:
         while n % divisor == 0:
             operations += divisor
