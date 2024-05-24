3c37cc55ca5d7a827678b83d93b80429d42af179
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 04:01:57 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,18 +16,21 @@ def validUTF8(data):
     mask1 = 1 << 7    # 10000000
     mask2 = 1 << 6    # 01000000
     for num in data:
+        # Mask to get the last 8 bits
         byte = num & 0xFF
         if num_bytes == 0:
+            # Count the number of leading 1 bits
             mask = 1 << 7
             while mask & byte:
                 num_bytes += 1
                 mask >>= 1
             if num_bytes == 0:
                 continue
-                if num_bytes == 1 or num_bytes > 4:
-                    return False
-                else:
-                    if not (byte & mask1 and not (byte & mask2)):
-                        return False
-                        num_bytes -= 1
-                        return num_bytes == 0
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            # Check that the byte is of form 10xxxxxx
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
