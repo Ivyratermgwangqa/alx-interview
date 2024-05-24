ba77a454cb11c5e8249e053df2e0f948c4449d0f
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-24 03:59:57 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,7 +17,6 @@ def validUTF8(data):
     mask2 = 1 << 6    # 01000000
     for num in data:
         byte = num & 0xFF
-        
         if num_bytes == 0:
             mask = 1 << 7
             while mask & byte:
@@ -25,13 +24,10 @@ def validUTF8(data):
                 mask >>= 1
             if num_bytes == 0:
                 continue
-            
-            if num_bytes == 1 or num_bytes > 4:
-                return False
-        else:
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-        
-        num_bytes -= 1
-    
-    return num_bytes == 0
+                if num_bytes == 1 or num_bytes > 4:
+                    return False
+                else:
+                    if not (byte & mask1 and not (byte & mask2)):
+                        return False
+                        num_bytes -= 1
+                        return num_bytes == 0
